009497eec2ee262728cf45db43ee4c5fac923bff
Octavio Oyanedel <octavio.oyanedel@gmail.com>
2020-01-17 03:34:28 +0000

    Commit inicial

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        @include('includes.css_mdb')
+
+    </head>
+    <body>
+        <div class="flex-center position-ref full-height">
+
+            <div class="content">
+            <select class="browser-default custom-select">
+              <option selected>Open this select menu</option>
+              <option value="1">One</option>
+              <option value="2">Two</option>
+              <option value="3">Three</option>
+            </select>
+            </div>
+        </div>
+        @include('includes.js_mdb')
+    </body>
+</html>
+
+
+ git remote add origin https://github.com/octavioOyanedel/kinoparatodos.git
+
